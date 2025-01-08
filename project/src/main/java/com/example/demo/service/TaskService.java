5bcce4a531269c3a341b49cb6eab6770c91422eb
Brunoo47 <brunoostroskiprogramador@gmail.com>
2025-01-07 22:36:43 -0300

    Create Entity, Service and

--- project/src/main/java/com/example/demo/service/TaskService.java
+++ project/src/main/java/com/example/demo/service/TaskService.java
@@ -3,9 +3,11 @@ package com.example.demo.service;
 import com.example.demo.domain.model.EntityTask;
 import com.example.demo.repository.TasksRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.config.Task;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TaskService {
@@ -14,5 +16,15 @@ public class TaskService {
 
     public List <EntityTask> getAllTasks() {
         return tasksRepository.findAll();
+    }
+    public Optional<EntityTask> getTaskById(Long id) {
+        return tasksRepository.findById(id);
+    }
+    public Task updateTask(Long id, Task newTask) {
+        getTaskById(id);
+        return tasksRepository.save(Task);
+    }
+    public void deleteTask(Long id) {
+        tasksRepository.deleteById(id);
     }
 }
