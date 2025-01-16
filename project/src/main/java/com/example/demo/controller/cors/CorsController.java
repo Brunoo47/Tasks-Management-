eb26f3e3b0b23dc82c55379d3faf13b55a7a97c1
Brunoo47 <brunoostroskiprogramador@gmail.com>
2025-01-16 20:49:58 -0300

    edit

--- project/src/main/java/com/example/demo/controller/cors/CorsController.java
+++ project/src/main/java/com/example/demo/controller/cors/CorsController.java
@@ -12,19 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
-@RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:4200")
-public class CorsController {
-    @GetMapping("/EntityTask")
-    public ResponseEntity<List>EntityTask(){
-        return ResponseEntity.ok().body(new ArrayList<>());
-    }
 @Configuration
-    public class WebConfig implements WebMvcAutoConfiguration {
-        @Override
-    public void addCorsMappings(CorsRegistry registry);
-}
+public class CorsController extends WebMvcAutoConfiguration {
+    @Override
+    public void AddCorsMapping(CorsRegistry registry){
+        registry.addMaping("/").
+    }
 
 }
-
